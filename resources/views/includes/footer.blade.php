42ba9a45340c21f4882001f2f6f50efb0986b0b9
iheb-heni <ihebheni013@gmail.com>
2025-08-22 22:38:17 +0100

    rani bdit tawa barka

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,5 @@
+   <footer>
+        <div class="text-center mt-5">
+            <p class="text-muted">© 2025 Laravel Demo. All rights reserved.</p>
+        </div>
+    </footer>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>laravel demo</title>
+    <style>
+        .text-orange {
+            color: orange !important;
+        }
+
+        footer{
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+            background-color: #f8f9fa;
+            padding: 10px 0;
+        }
+    </style>
+</head>
+
+<body>
+
+@include('includes.navbar')
+    <h1 class="text-center mt-5">welcome to laravel tuto</h1>
+
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit blanditiis accusamus quisquam cupiditate dolorum deserunt atque fugit doloribus officiis quidem, veniam maiores quae id assumenda eveniet est recusandae, quis expedita, possimus libero ex commodi ab perspiciatis. Debitis explicabo, aliquid totam atque sit dolorem nihil est?
+        Velit et, repellat temporibus alias libero molestiae iste dolorum fugiat corrupti odit, iure sunt quia facilis aut, ad veritatis. In eaque sequi ratione quaerat unde illum. At rerum sequi nam velit harum aliquam assumenda explicabo omnis quia animi? Iusto, architecto animi libero asperiores fuga est.
+        Quidem ratione placeat alias sint ipsum consectetur labore quaerat vitae in asperiores, repudiandae rem! Illo nemo commodi omnis sunt exercitationem incidunt earum, asperiores dolores, porro saepe eligendi. Doloremque saepe, reiciendis alias itaque veritatis repudiandae laborum? Esse ipsam molestias error aliquam reprehenderit quaerat doloremque id minus!
+        Velit quisquam officiis maxime aut nisi minima et veritatis praesentium, exercitationem ea. Beatae, provident, velit, reprehenderit eaque facere accusantium in tempora veritatis iusto eligendi iste molestiae. Neque assumenda eius voluptatibus natus iste. Illo blanditiis impedit amet, autem tempora cumque assumenda, fugiat officia iste reiciendis eius!
+        Sed, sapiente distinctio odit at doloribus optio. Dolores rem iure odio ab optio, rerum, nihil porro similique eaque magnam expedita pariatur quaerat, est illo? Assumenda, dolor? Qui quo repellat inventore, nihil voluptate earum necessitatibus dolorem in laborum amet labore sint saepe doloremque architecto eveniet? Dolorem.
+        Maxime nisi aut velit. Natus saepe, iste voluptates ut optio non accusamus eaque quos maiores consequatur rem nisi nam nesciunt cum voluptatum est, ipsa laboriosam qui minima officiis. Ratione nisi veniam, obcaecati fugit laborum, in nemo iure, tenetur officia esse rem excepturi vitae harum nihil?
+        Earum, laudantium beatae temporibus, unde corporis quis debitis, quo eos reiciendis voluptatem aliquam. Quaerat odio libero perferendis laborum repudiandae, at asperiores iusto quia reprehenderit ea autem beatae fugit, voluptatibus voluptatem exercitationem tempora incidunt! Inventore, sunt! Ea rem culpa ipsum sunt, est aspernatur non delectus iste.
+        Dolor consectetur itaque iure dignissimos voluptas rem deserunt, eveniet, eum, alias officiis recusandae aspernatur nulla praesentium numquam ad delectus tenetur culpa adipisci. Beatae, magnam deleniti! Eaque possimus consequuntur dignissimos inventore cumque! Nulla provident enim voluptate adipisci incidunt, nihil error dolorem perferendis qui, asperiores sed voluptates?
+        Voluptate commodi sint quia ducimus corporis distinctio beatae. Nesciunt odit deleniti eius at. Provident, eum optio id, necessitatibus maiores nesciunt aspernatur explicabo officia quibusdam libero odit laborum, assumenda itaque voluptatum debitis magnam perferendis velit. Mollitia dignissimos ad aliquid doloribus rerum praesentium debitis error minus quo.
+        Molestias neque dicta impedit harum ipsum iure aliquam optio obcaecati corporis dolorum non, commodi atque? Fuga possimus est, iste voluptatibus veniam, tempore rem tenetur voluptatem eius necessitatibus unde, nam vero harum quos quaerat fugiat ex similique corrupti consectetur fugit earum? Minus tempora molestiae quam facere!
+    </p>
+@include('includes.footer')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
